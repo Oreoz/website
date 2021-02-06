cd91b89ee72499db112608e8b49334016f20879a
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2021-02-06 11:07:00 -0500

    Tests ⚡

---
+++ cypress/integration/landing-page.spec.ts
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+const viewports: Cypress.ViewportPreset[] = [
+  "iphone-5",
+  "iphone-8",
+  "iphone-x",
+  "samsung-s10",
+  "ipad-2",
+  "macbook-13",
+  "macbook-15",
+];
+
+viewports.forEach((viewport) => {
+  describe(`index (${viewport})`, () => {
+    beforeEach(() => {
+      cy.viewport(viewport);
+      cy.visit("http://localhost:3000");
+    });
+
+    it("displays my name and story", () => {
+      cy.contains("Jean-Philippe Roy").should("be.visible");
+
+      cy.get('[data-test-id="paragraph-1"]').should("exist");
+      cy.get('[data-test-id="paragraph-2"]').should("exist");
+      cy.get('[data-test-id="paragraph-3"]').should("exist");
+    });
+
+    it("displays my avatar", () => {
+      cy.get('[data-test-id="avatar"]').should("be.visible");
+    });
+
+    it("displays social icons", () => {
+      cy.get('[data-test-id="linkedin"]').should("be.visible");
+      cy.get('[data-test-id="github"]').should("be.visible");
+    });
+
+    it("allows the user to switch themes", () => {
+      cy.get("html").should("not.have.class", "dark");
+
+      cy.get(`[data-test-id="theme-switch"]`)
+        .should("be.visible")
+        .should("have.text", "🦉")
+        .click()
+        .should("have.text", "🦚");
+
+      cy.get("html").should("have.class", "dark");
+    });
+  });
+});
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -25,7 +25,11 @@ const ThemeSwitch: FC = () => {
   const handleOnClick = () => setTheme(theme === "dark" ? "light" : "dark");
 
   return (
-    <button className="text-3xl p-1 m-1" onClick={handleOnClick}>
+    <button
+      data-test-id="theme-switch"
+      className="text-3xl p-1 m-1"
+      onClick={handleOnClick}
+    >
       {emoji}
     </button>
   );
