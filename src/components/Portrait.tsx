27ed5f729eb7e0208b07eccf7c7f537cf28bae93
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2021-03-19 20:56:07 -0400

    Explicit `height` and `width`

--- src/components/Portrait.tsx
+++ src/components/Portrait.tsx
@@ -3,7 +3,9 @@ import React, { FC } from "react";
 const Portrait: FC = () => {
   return (
     <img
-      className="rounded-full w-50 max-w-300 max-w-xs w-2/3 mb-4"
+      className="rounded-full max-w-300 max-w-xs w-2/3 mb-4"
+      height="250"
+      width="250"
       data-test-id="avatar"
       src="assets/me.jpg"
       alt="portrait"
