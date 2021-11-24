c311a13d6b9b0b8af238314491857f073bc2c384
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2021-11-24 18:50:49 -0500

    Tweaks Cypress Tests

--- cypress/integration/resume.spec.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("resume", () => {
-  beforeEach(() => cy.visit("http://localhost:3000/resume"));
-
-  it("is navigatable", () => {
-    cy.url().should("contain", "resume");
-  });
-});
