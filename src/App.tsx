ac78d72a3302e5e78c20cd054108a4ff299a1365
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2025-03-08 19:43:06 -0500

    refactor: decompose ui into components (#1168)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import RotatingText from "react-rotating-text";
 import { motion } from "framer-motion";
-import Nav from "./components/Nav";
+import { Socials } from "./components/socials";
+import { ThemeSwitch } from "./components/theme-switch";
+import { Bio } from "./components/bio";
 
 export default function App() {
   return (
@@ -12,7 +14,9 @@ export default function App() {
         transitionTimingFunction: "linear",
       }}
     >
-      <Nav />
+      <nav className="flex justify-end items-center">
+        <ThemeSwitch />
+      </nav>
 
       <div className="flex flex-col items-center max-w-screen-md mx-auto p-6">
         <motion.img
@@ -40,58 +44,9 @@ export default function App() {
           ]}
         />
 
-        <div className="flex flex-col mt-4">
-          <p className="text-justify mb-3" data-test-id="paragraph-1">
-            I am a software engineer, passionate about JavaScript & building
-            quality software. Most of my work so far has been done using modern
-            JavaScript in the browser using Ember.js and React.
-          </p>
+        <Bio />
 
-          <p className="text-justify mb-3" data-test-id="paragraph-2">
-            Outside of programming and taking care of my little one. I enjoy
-            playing video games, board games, watching movies, listening to
-            music, cycling & drinking craft beer with friends.
-          </p>
-
-          <p className="text-justify mb-3" data-test-id="paragraph-3">
-            If you&apos;re looking to contact me for profesionnal inquiries,
-            your best bet would be LinkedIn.
-          </p>
-        </div>
-
-        <div className="flex mt-4">
-          <motion.a
-            data-test-id="linkedin"
-            href="https://www.linkedin.com/in/jean-philippe-roy-64918658"
-            rel="noopener noreferrer"
-            target="_blank"
-            whileHover={{ scale: 1.1 }}
-          >
-            <span
-              role="img"
-              aria-label="LinkedIn"
-              className="social-shapes text-4xl mx-1"
-            >
-              k
-            </span>
-          </motion.a>
-
-          <motion.a
-            data-test-id="github"
-            href="https://github.com/Oreoz"
-            rel="noopener noreferrer"
-            target="_blank"
-            whileHover={{ scale: 1.1 }}
-          >
-            <span
-              role="img"
-              aria-label="GitHub"
-              className="social-shapes text-4xl mx-1"
-            >
-              Y
-            </span>
-          </motion.a>
-        </div>
+        <Socials />
       </div>
     </div>
   );
